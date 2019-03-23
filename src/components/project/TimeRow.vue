4f51ddb487fd05f1a49729ad2bf1531eb3c5936c
Martin Matejovsky <matejovsky.ma@seznam.cz>
2019-03-23 17:05:09 +0100

    Renanimg Vue components from Xy to XyComponent

--- src/components/project/TimeRow.vue
+++ src/components/project/TimeRow.vue
@@ -1,26 +1,26 @@
 <template>
     <div class="c-time-row">
-        <Checkbox type="checkbox" :inputModel="rowIsAffectiongCaltulation" />
-        <Input type="number" />
-        <Input type="number" />
-        <Input type="number" />
+        <CheckboxComponent type="checkbox" :inputModel="rowIsAffectiongCaltulation" />
+        <InputComponent type="number" />
+        <InputComponent type="number" />
+        <InputComponent type="number" />
         <div class="time-row-controller">
-            <Button label="-" classCustom="button-row-controller is-minus" />
-            <Button label="+" classCustom="button-row-controller is-plus" />
+            <ButtonComponent label="-" classCustom="button-row-controller is-minus" />
+            <ButtonComponent label="+" classCustom="button-row-controller is-plus" />
         </div>
     </div>
 </template>
 
 <script>
-    import Input from '../form/Input'
-    import Button from "../form/Button";
-    import Checkbox from "../form/Checkbox";
+    import InputComponent from '../form/InputComponent'
+    import ButtonComponent from "../form/ButtonComponent";
+    import CheckboxComponent from "../form/CheckboxComponent";
 
     export default {
         components: {
-            Checkbox,
-            Button,
-            Input
+            CheckboxComponent,
+            ButtonComponent,
+            InputComponent
         },
         data() {
             return {
